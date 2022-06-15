147ab1a5647e273c0d78aeb18db134ba9d16016f
SeTheory <476017949@qq.com>
2022-06-15 22:13:27 +0800

    init

---
+++ main.py
@@ -0,0 +1,110 @@
+import argparse
+import datetime
+import os
+import pandas as pd
+from bs4 import BeautifulSoup
+import json
+
+def get_valid_ref_list(soup):
+    result_dict = {}
+    ref_list = soup.find('ref-list')
+    if ref_list:
+        ref_list = ref_list.find_all('ref')
+        for ref in ref_list:
+            ref_label = ref.attrs['id']
+            ref_ids = ref.find_all('pub-id')
+            if len(ref_ids) < 1:
+                continue
+    #         print(ref_ids)
+            result_dict[ref_label] = {ref_id.attrs['pub-id-type']: ref_id.text  for ref_id in ref_ids}
+    return result_dict
+
+
+def get_article_meta(soup):
+    article = soup.front.find('article-meta')
+    if article.find('kwd-group'):
+        kwds = [kwd.text for kwd in article.find('kwd-group').find_all('kwd')]
+    else:
+        kwds = []
+
+    if article.find('abstract'):
+        abstract = article.find('abstract').text  # 没清理过的latex内容，需要进一步处理
+    else:
+        abstract = ''
+
+    pub_date = {'year': article.find('pub-date').find('year').text if article.find('pub-date').find('year') else None,
+                'month': article.find('pub-date').find('month').text if article.find('pub-date').find(
+                    'month') else None,
+                'day': article.find('pub-date').find('day').text if article.find('pub-date').find('day') else None}
+
+    if article.find('contrib-group'):
+        authors = article.find('contrib-group').find_all('contrib')
+        authors = [{'type': contrib.attrs, 'name': {name.name: name.text for name in contrib.find('name').children}} for
+                   contrib in authors]
+        aff = [str(item) for item in article.find('contrib-group').find_all('aff')]  # 没想好怎么处理机构
+    else:
+        authors = []
+        aff = []
+
+    cat = article.find('article-categories').subject.text if article.find('article-categories') else None
+    ids = {arc_id.attrs['pub-id-type']: arc_id.text for arc_id in article.find_all('article-id')}
+    title = article.find('article-title').text
+
+    journal = soup.front.find('journal-meta')
+    journal_ids = {jid.attrs['journal-id-type']: jid.text for jid in journal.find_all('journal-id')}
+    journal_title = journal.find('journal-title').text
+
+    if soup.body:
+        full = len(soup.body.find_all('xref', attrs={'ref-type': 'bibr'})) > 0
+    else:
+        full = False
+
+    return {
+        'pmc': ids['pmc'],
+        'ids': ids,
+        'title': title,
+        'cat': cat,
+        'aff': aff,
+        'authors': authors,
+        'pub_date': pub_date,
+        'abstract': abstract,
+        'kwds': kwds,
+        'journal': {'ids': journal_ids, 'title': journal_title},
+        'full': full
+    }
+
+
+def get_simple_data(data_path, name):
+    ref_dict = {}
+    pub_dict = {}
+    folder_list = os.listdir(data_path)
+    for folder in folder_list:
+        folder_path = data_path + folder
+        files = os.listdir(folder_path)
+        for file in files:
+            file_path = folder_path + '/' + file
+            #         print(file_path)
+            with open(file_path) as fr:
+                sample = fr.readlines()
+            soup = BeautifulSoup(''.join(sample), ["lxml-xml"])
+            temp_ref = get_valid_ref_list(soup)
+            temp_pub = get_article_meta(soup)
+            cur_pmc = temp_pub['pmc']
+            ref_dict[cur_pmc] = temp_ref
+            pub_dict[cur_pmc] = temp_pub
+    json.dump(ref_dict, open('./data/{}.ref'.format(name), 'w+', encoding='utf-8'))
+    json.dump(pub_dict, open('./data/{}.pub'.format(name), 'w+', encoding='utf-8'))
+
+if __name__ == "__main__":
+    start_time = datetime.datetime.now()
+    parser = argparse.ArgumentParser(description='Process some description.')
+    parser.add_argument('--phase', default='test', help='the function name.')
+    parser.add_argument('--data_path', default=None, help='the input.')
+    parser.add_argument('--name', default=None, help='file name.')
+    # parser.add_argument('--out_path', default=None, help='the output.')
+    # parser.add_argument('--seed', default=123, help='the seed.')
+    args = parser.parse_args()
+    if args.phase == 'test':
+        print('This is a test process.')
+    elif args.phase == 'simple_data':
+        get_simple_data(args.data_path, args.name)
