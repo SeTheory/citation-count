03e250a20112a0274c93541419ad3f9624d8a47e
SeTheory <476017949@qq.com>
2022-06-22 18:42:32 +0800

    update

--- s2orc.py
+++ s2orc.py
@@ -1,10 +1,39 @@
 import argparse
 import datetime
+import json
 import os
 
 
 def reorganize_data(data_path):
     files = os.listdir(data_path)
+    info_fw = open(data_path + 'all_info.lines', 'w+')
+    ab_fw = open(data_path + 'all_abstract.lines', 'w+')
+    citation_fw = open(data_path + 'all_cite.lines', 'w+')
+    reference_fw = open(data_path + 'all_ref.lines', 'w+')
+    for file in files:
+        with open(data_path + file) as fr:
+            for line in fr:
+                temp_data = json.loads(line)
+                temp_info = {
+                    'paper_id': temp_data['paper_id'],
+                    'title': temp_data['title'],
+                    'authors': temp_data['authors'],
+                    'year': temp_data['year'],
+                    'journal': temp_data['journal'],
+                    'venue': temp_data['venue'],
+                    'mag_field_of_study': temp_data['mag_field_of_study'],
+                    'citations': len(temp_data['inbound_citations']),
+                    'has_pdf_parse': temp_data['has_pdf_parse'],
+                    'file_name': file
+                }
+                info_fw.write(str(temp_info) + '\n')
+                ab_fw.write(str([temp_data['paper_id'], temp_data['abstract']]) + '\n')
+                citation_fw.write(str([temp_data['paper_id'], temp_data['inbound_citations']]) + '\n')
+                reference_fw.write(str([temp_data['paper_id'], temp_data['outbound_citations']]) + '\n')
+    info_fw.close()
+    ab_fw.close()
+    citation_fw.close()
+    reference_fw.close()
 
 
 if __name__ == "__main__":
@@ -18,3 +47,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.phase == 'test':
         print('This is a test process.')
+    if args.phase == 'reorganize_data':
+        reorganize_data(args.data_path)
+        print('reorganize data done.')
